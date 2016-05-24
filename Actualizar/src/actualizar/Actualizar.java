c76ceb35a485663c70dc6d623f96003d5240142b
victorM66 <victorstv1@gmail.com>
2016-05-24 11:44:22 -0600

    Update Actualizar.java

--- Actualizar/src/actualizar/Actualizar.java
+++ Actualizar/src/actualizar/Actualizar.java
@@ -19,6 +19,7 @@ public class Actualizar {
      */
     public static void main(String[] args) {
       JOptionPane.showMessageDialog(null, "actualizar");
+      JOptionPane.showMessegeDialog(null, "Hola mundo :v");
     }
     
 }
